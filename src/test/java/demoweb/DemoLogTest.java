692d4c7573abb750714d18527a5f1b78e97ba2de
unknown <saranaveen1997@gmail.com>
2024-10-29 11:36:53 +0530

    Src changes committed

--- src/test/java/demoweb/DemoLogTest.java
+++ src/test/java/demoweb/DemoLogTest.java
@@ -3,6 +3,7 @@ package demoweb;
 import java.io.File;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -10,7 +11,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -26,9 +26,11 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class DemoLogTest {
+	
+	public static final String username="saraswathi_OWIZrj";
+	public static final String access_KEY="BuyiuACwXgVC4d7UB1rv";
+	public static final String URL ="https://"+username+":"+access_KEY+"@hub-cloud.browserstack.com/wd/hub";
 	private ExtentReports extent;
 	private ExtentTest test;
 	
@@ -47,14 +49,30 @@ public class DemoLogTest {
 		  
 		  // Optionally, you can add Chrome options if needed 
 		  ChromeOptions options =new ChromeOptions(); // Add any specific options if required //
-		  options.addArguments("--headless"); // Uncomment for headless mode
-		  DesiredCapabilities capabilities = new DesiredCapabilities(); 
-		  capabilities.setBrowserName("chrome");
-		  capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		  //options.addArguments("--headless"); // Uncomment for headless mode
+		 // DesiredCapabilities capabilities = new DesiredCapabilities(); 
+		  //capabilities.setBrowserName("chrome");
+		  //capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+			/*
+			 * capabilities.setCapability("os","windows");
+			 * capabilities.setCapability("os_version","11");
+			 * capabilities.setCapability("browser","chrome");
+			 * capabilities.setCapability("browser_version","106");
+			 * capabilities.setCapability("name","Testcase for Login Module");
+			 */
 		  
+		    options.setCapability("platformName", "Windows");
+		    options.setCapability("browserVersion", "106");
+		    options.setCapability("browserName", "chrome");
+		    options.setCapability("bstack:options", Map.of(
+		        "os", "Windows",
+		        "osVersion", "11",
+		        "projectName", "Login Module Test",
+		        "sessionName", "Testcase for Login Module"
+		    ));
 		  // Initialize the remote WebDriver pointing to the Selenium Grid hub //
-		  driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
-		  
+		  //driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+		  driver=new RemoteWebDriver(new URL(URL), options);  
 		extent = new ExtentReports();
 		ExtentSparkReporter sparkReporter = new ExtentSparkReporter("extent-report.html");
 		extent.attachReporter(sparkReporter);
@@ -109,7 +127,7 @@ public class DemoLogTest {
 	public void tearDown() {
 		extent.flush();
 		if (driver != null) {
-			driver.quit();
+			driver.close();
 		}
 	}
 }
